c215acc98bca6246d727b970405c423bba590ba9
Monsterball13 <99062448+Monsterball13@users.noreply.github.com>
2023-04-13 19:22:00 +0530

    Add files via upload

---
+++ Client/calculation.js
@@ -0,0 +1,10 @@
+function calculate() {
+
+    var x = document.getElementById('cost').value;
+    var y = document.getElementById('total').value;
+    y= x++;
+    var z = document.getElementById('percent').value;
+
+    z= x/y*100;
+
+}
